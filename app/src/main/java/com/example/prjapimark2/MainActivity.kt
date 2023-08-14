0f329ead9300bf67ae38c8b401455e8f8771a4d6
Reece Wanvig <rwanvig@iie.ac.za>
2023-08-14 11:50:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/prjapimark2/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.prjapimark2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.TextView
+import com.github.kittinunf.fuel.core.extensions.jsonBody
+import com.github.kittinunf.fuel.httpPost
+import com.google.gson.Gson
+import java.net.URL
+import java.util.concurrent.Executors
+data class PostUser(var ID: String, var Amount: String,var PracticeID:String)
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Post()
+    }
+    fun Read()
+    {
+        val executor = Executors.newSingleThreadExecutor()
+        executor.execute{
+            val url = URL ("https://wordapidata.000webhostapp.com/?getuserdb");
+            val json = url.readText()
+            val userList = Gson().fromJson(json,Array<User>::class.java).toList()
+
+            Handler(Looper.getMainLooper()).post{
+                Log.d("AddNewUser", "Plain Json Vars :" + json.toString())
+                Log.d("AddNewUser", "Converted Json :" + userList.toString())
+            }
+
+        }
+    }
+
+
+    fun Post()
+    {
+        val executor = Executors.newSingleThreadExecutor()
+        executor.execute{
+            val user = PostUser("15", "50000","123456")
+
+            val (_, _, result) = "https://opsc.azurewebsites.net/Add.php".httpPost()
+                .jsonBody(Gson().toJson(user).toString())
+                .responseString()
+
+            Handler(Looper.getMainLooper()).post{
+                var Text = findViewById<TextView>(R.id.txtOutput)
+                Text.setText(result.toString())
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/prjapimark2/User.kt
@@ -0,0 +1,7 @@
+package com.example.prjapimark2
+
+data class User(
+    val Name:String,
+    val Password:String,
+    val imageURL:String
+)
